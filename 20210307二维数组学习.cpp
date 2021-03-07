620b34ee6391dad43b8e031523d8a252bfa1e3c7
mk <2427578002@qq.com>
2021-03-07 23:04:16 +0800

    二维数组学习 数组交换

---
+++ 20210307二维数组学习.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	int a[2][3] = { { 1, 2, 3 }, { 4, 5, 6 } };
+	int b[3][2], i, j;
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d  ", a[i][j]);
+			b[j][i] = a[i][j];
+		}
+		printf("\n");
+	}
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			printf("%d   ", b[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+
+}
